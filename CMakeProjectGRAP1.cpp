67b8832b702a078bbc4201d313a0ef51ad58a811
embdevv <111500829+embdevv@users.noreply.github.com>
2026-01-07 17:11:52 +0800

    added triangles

--- CMakeProjectGRAP1.cpp
+++ CMakeProjectGRAP1.cpp
@@ -9,7 +9,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Erica Barundia", NULL, NULL); 
+    window = glfwCreateWindow(640, 480, "Erica Barundia", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -26,9 +26,50 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
+		glVertex2f(-0.5f, -0.5f); // a
+        glVertex2f(0.0f, 0.5f);   // b
+		glVertex2f(0.5f, -0.5f);  // c
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-1, 0);       // d
+        glVertex2f(0.0f, 0.5f);  // b
+        glVertex2f(-0.5f, -0.5f); // a
+        
+        glBegin(GL_TRIANGLES); // FBD
+        glVertex2f(-1.1f, 0.6f); // f
+        glVertex2f(0.0f, 0.5f);  // b
+        glVertex2f(-1, 0);       // d
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-1.1f, 0.6f); // f
+        glVertex2f(0.0f, 0.5f);  // b
+        glVertex2f(-0.75f, 1.2f); // h
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.75f, 1.2f); // h
+        glVertex2f(0.0f, 0.5f);   // b
+        glVertex2f(0, 1.5f);      // i
+
+		glBegin(GL_TRIANGLES); // IBJ
+        glVertex2f(0, 1.5f);      // i
+        glVertex2f(0.0f, 0.5f);   // b
+        glVertex2f(0.75f, 1.2f);  // j
+
+        glBegin(GL_TRIANGLES); // JBG
+		glVertex2f(0.75f, 1.2f);  // j
+		glVertex2f(0.0f, 0.5f);   // b
+		glVertex2f(1.1f, 0.6f);  // g
+
+        glBegin(GL_TRIANGLES); // GBE
+		glVertex2f(1.1f, 0.6f);  // g
+		glVertex2f(0.0f, 0.5f);   // b
+		glVertex2f(1, 0);        // e
+
+        glBegin(GL_TRIANGLES); // EBC
+		glVertex2f(1, 0);        // e
+		glVertex2f(0.0f, 0.5f);   // b
+		glVertex2f(0.5f, -0.5f);  // c
+
         glEnd();
 
         /* Swap front and back buffers */
