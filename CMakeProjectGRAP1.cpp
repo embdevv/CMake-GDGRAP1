3e78aff04b07d7e427b9e87a641fd026d376400a
embdevv <111500829+embdevv@users.noreply.github.com>
2026-01-12 16:32:56 +0800

    Added glad & applied changes
    Converted GL vertex data to glad using glVertexArray and glBufferData functions.

--- CMakeProjectGRAP1.cpp
+++ CMakeProjectGRAP1.cpp
@@ -1,4 +1,5 @@
-﻿#include <GLFW/glfw3.h>
+﻿#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 #include <math.h>
 
 #ifndef M_PI
@@ -20,73 +21,34 @@ int main(void)
     }
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+	gladLoadGL(glfwGetProcAddress);
+
+    GLfloat vertices[]{
+        0.f,  0.5f, 0.f,  //> (0, 0.5, 0)    top vertex
+        -1.f, -0.5f, 0.f, //> (-1, -0.5, 0)  bottom left verteex
+        0.5f, -0.5f, 0.f  //> (0.5, -0.5, 0) bottom right vertex
+    };
+
+    GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+    // Strictly in order
+
+    // currVAO = null
+	glBindVertexArray(VAO);
+    // currVAO = VAO
+
+	// currVBO = null
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// currVBO = VBO <- GL_ARRAY_BUFFER
+    // currVAO.push(currVBO)
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // Calculate nonagon vertices
-        float radius = 0.9f;
-        float centerX = 0.0f;
-        float centerY = 1.0f - radius; // Position so top vertices touch y=1.0
-        float angleOffset = M_PI / 2.0f + M_PI / 9.0f; // Top side horizontal
-
-        float vertices[9][2];
-        for (int i = 0; i < 9; i++) {
-            float angle = angleOffset + (2.0f * M_PI * i) / 9.0f;
-            vertices[i][0] = centerX + radius * cos(angle);
-            vertices[i][1] = centerY + radius * sin(angle);
-        }
-
-        // Draw triangles from center to each edge
-        glBegin(GL_TRIANGLES);
-        glVertex2f(centerX, centerY);
-        glVertex2f(vertices[0][0], vertices[0][1]);
-        glVertex2f(vertices[1][0], vertices[1][1]);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(centerX, centerY);
-        glVertex2f(vertices[1][0], vertices[1][1]);
-        glVertex2f(vertices[2][0], vertices[2][1]);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(centerX, centerY);
-        glVertex2f(vertices[2][0], vertices[2][1]);
-        glVertex2f(vertices[3][0], vertices[3][1]);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(centerX, centerY);
-        glVertex2f(vertices[3][0], vertices[3][1]);
-        glVertex2f(vertices[4][0], vertices[4][1]);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(centerX, centerY);
-        glVertex2f(vertices[4][0], vertices[4][1]);
-        glVertex2f(vertices[5][0], vertices[5][1]);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(centerX, centerY);
-        glVertex2f(vertices[5][0], vertices[5][1]);
-        glVertex2f(vertices[6][0], vertices[6][1]);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(centerX, centerY);
-        glVertex2f(vertices[6][0], vertices[6][1]);
-        glVertex2f(vertices[7][0], vertices[7][1]);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(centerX, centerY);
-        glVertex2f(vertices[7][0], vertices[7][1]);
-        glVertex2f(vertices[8][0], vertices[8][1]);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(centerX, centerY);
-        glVertex2f(vertices[8][0], vertices[8][1]);
-        glVertex2f(vertices[0][0], vertices[0][1]);
-
-        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
